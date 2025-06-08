9c5c9c28999dd018fd1059bf451d234fbeab15c3
algi00 <familyayanh@gmail.com>
2025-06-08 19:00:36 +0700

    Update config (5).py

--- PyroUbot/config (5).py
+++ PyroUbot/config (5).py
@@ -3,23 +3,23 @@ from dotenv import load_dotenv
 
 load_dotenv(".env")
 
-MAX_BOT = int(os.getenv("MAX_BOT", "60"))
+MAX_BOT = int(os.getenv("MAX_BOT", "20"))
 
-DEVS = list(map(int, os.getenv("DEVS", "5343384013").split()))
+DEVS = list(map(int, os.getenv("DEVS", "7524271223").split()))
 
-API_ID = int(os.getenv("API_ID", "26297818"))
+API_ID = int(os.getenv("API_ID", "21756016"))
 
 API_HASH = os.getenv("API_HASH", "4a48374f0fd3549af86a7e5fba563b8f")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7737623053:AAEf3brYvrsvvz77tmMO8N3ngre1PSIfJ3Y")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7822314667:AAGpsnA0ePJbfAynO-0evSXNH8uOYd_2F7Q")
 
-OWNER_ID = int(os.getenv("OWNER_ID", "5343384013"))
+OWNER_ID = int(os.getenv("OWNER_ID", "6621603339"))
 
-BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002125842026 -1002053287763 -1002044997044 -1002022625433 -1002050846285").split()))
+BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002718575966 -1002053287763 -1002044997044 -1002022625433 -1002050846285").split()))
 
-RMBG_API = os.getenv("RMBG_API", "a6qxsmMJ3CsNo7HyxuKGsP1o")
+RMBG_API = os.getenv("RMBG_API", "cdda26fd6b87e7fb35f1b8d0d28d7cd6")
 
 MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://zan:zan@cluster0.epnnx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1002367286705"))
+LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1002718575966"))
 
